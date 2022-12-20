09b19e56ce0a3f2f65b64abaef023c4d0e3ad791
Abu Shama <assiddik001@gmail.com>
2022-12-20 22:39:47 +0600

    db connection

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,23 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv").config();
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "mysql",
+});
+
 const DBConnect = async () => {
-  console.log("connected");
-  return;
+  try {
+    await sequelize.authenticate();
+    console.log(`==== DB Connection is succesfully ====`.green.bold.italic);
+  } catch (error) {
+    console.log(`==== Database Connection Error ====`.red.bold.italic, error);
+  }
 };
 
 module.exports = DBConnect;
